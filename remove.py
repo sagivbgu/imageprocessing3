17ca1b14f26cd10a3041ff0fc73483586df9e3b8
sagivbgu <sagivsc@post.bgu.ac.il>
2020-12-27 21:57:20 +0200

    Code review (added TODOs)

--- remove.py
+++ remove.py
@@ -1,5 +1,8 @@
 import cv2
 
+HEIGHT_TO_WIDTH_RATIO_THRESHOLD = 1.8  # TODO: I've extracted this const. Check if it makes sense to you, and if
+                                       #  there are more constants we can extract. Apparently Jihad loves control ;)
+
 GREEN = (0, 255, 0)
 RED = (0, 0, 255)
 
@@ -48,6 +51,7 @@ def apply_otsu(img):
 def get_contours(img):
     # find only the extreme outer contours (connected components: letters and demarcation)
     # using chain approximation method
+    # TODO: Maybe using cv2.RETR_LIST we can identify Dagesh-s inside letters?
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     return contours
@@ -57,7 +61,7 @@ def sort_contours_by_height(contours):
     contours_sizes = []
     for index, cnt in enumerate(contours):
         _, _, _, height = cv2.boundingRect(cnt)
-        contours_sizes.append([index, height, cnt])
+        contours_sizes.append([index, height, cnt])  # TODO: Is it needed to save the cnt itself? Only its index and height are used
 
     # sort them by the height
     contours_sizes.sort(key=lambda x: x[1])
@@ -66,6 +70,7 @@ def sort_contours_by_height(contours):
 
 
 def get_index_of_max_difference(contours_sizes):
+    # TODO: Change comment (and code?) since we deal with heights rather than areas
     # In this method we iterate over the contours (sorted by area size)
     # and for each two adjacent sizes in the list we calculate the difference.
     # Then, we determine the index in which the difference is the most significant.
@@ -88,7 +93,7 @@ def filter_small_contours_by_width(indexes, contours):
     for index in indexes:
         _, _, w, h = cv2.boundingRect(contours[index])
         # if its much higher than its width, ignore it
-        if h >= 1.8 * w:
+        if h >= HEIGHT_TO_WIDTH_RATIO_THRESHOLD * w:
             pass
         else:
             new_indexes.append(index)
@@ -103,12 +108,16 @@ def erase_contours(img, indexes, contours):
 
 def erase_contour(img, contours, index):
     # erase the edges and a little bit outside the contour to avoid noise
-    cv2.drawContours(img, contours, index, BACKGROUND_COLOR, 2)
+    cv2.drawContours(img, contours, index, BACKGROUND_COLOR, 2)  # TODO: Extract '2' to a const?
     # erase the inner side of the contour
     cv2.drawContours(img, contours, index, BACKGROUND_COLOR, -2)
 
 
 def get_kamatzs(img, indexes, contours):
+    # TODO: Can we delete 'img' parameter?
+    # TODO: Remove comments. Then, Is the following line enough?
+    # return [index for index in indexes if is_kamatz(contours[index])]
+
     kamatzs = []
     # img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
     for index in indexes:
@@ -130,7 +139,7 @@ def is_kamatz(contour):
     perimeter = cv2.arcLength(contour, True)
     approx = cv2.approxPolyDP(contour, 0.005 * perimeter, True)
     _, _, w, h = cv2.boundingRect(contour)
-    if 9 <= len(approx) and abs(w - h) <= 2:
+    if 9 <= len(approx) and abs(w - h) <= 2:  # TODO: Extract '2','9' to consts?
         return True
     else:
         return False
